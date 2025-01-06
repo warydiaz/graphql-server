9b5a7975e0262d9751f2c0f929ae40cddb3b49a4
Angel Diaz <adiaz@vistagaming.com>
2025-01-06 21:13:57 +0100

    Add user model and authentication mutations; integrate JWT for login functionality

--- index.js
+++ index.js
@@ -1,106 +1,144 @@
-import { gql, ApolloServer, UserInputError } from 'apollo-server';
-import './db.js';
-import Person from './models/person.js';
+import { gql, ApolloServer, UserInputError } from "apollo-server";
+import "./db.js";
+import Person from "./models/person.js";
+import User from "./models/user.js";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const typeDefs = gql`
-    enum YesNo {
-        YES
-        NO
-    }
+  enum YesNo {
+    YES
+    NO
+  }
 
-    type Address {
-        street: String!
-        city: String!
-    }
+  type Address {
+    street: String!
+    city: String!
+  }
 
-    type Person {
-        name: String!
-        phone: String
-        address: Address!
-        birthYear: Int!
-        age: Int
-        id: ID!
-    }
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    birthYear: Int!
+    age: Int
+    id: ID!
+  }
 
-    type Mutation {
-        addPerson(
-            name: String!
-            phone: String
-            street: String!
-            city: String!
-            birthYear: Int!
-        ): Person
+  type User {
+    username: String!
+    friends: [Person]!
+    id: ID!
+  }
 
-        editNumber(
-            name: String!
-            phone: String!
-        ): Person
-    }
-    
-    type Query {
-        personCount: Int!
-        allPersons(phone: YesNo): [Person]!
-        findPerson(name: String!): Person
-    }`;
+  type Token {
+    value: String!
+  }
 
-    const savePersonData = async(person) => {
-        try {
-            await person.save();
-        } catch (error) {
-            throw new UserInputError(error.message, {
-                invalidArgs: person,
-            });
-        }
-    }
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+      birthYear: Int!
+    ): Person
 
-    const resolvers = {
-        Query: {
-            personCount: async () => await Person.collection.countDocuments(),
-            allPersons: async (root, args) => {
+    editNumber(name: String!, phone: String!): Person
 
-                if (!args.phone) return await Person.find({});
+    createUser(username: String!): User
 
-                return await Person.find({ phone: { $exists: args.phone === 'YES' } });
-            },
-            findPerson: async (root, args) => {
-                const name = args.name;
-                return await Person.findOne({
-                    name: name
-                });
-            }
-        },
-    
-        Mutation: {
-            addPerson: async (root, args) => {
-                const person = new Person({ ...args });
-                return await savePersonData(person);
-            },
-            editNumber: async (root, args) => {
-                const person = await Person.findOne({ name: args.name });
-                
-                if (!person) return null;
+    login(username: String!, password: String!): Token
+  }
 
-                person.phone = args.phone;
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person]!
+    findPerson(name: String!): Person
+    me: User
+  }
+`;
 
-                await savePersonData(person);
+const savePersonData = async (person) => {
+  try {
+    await person.save();
+  } catch (error) {
+    throw new UserInputError(error.message, {
+      invalidArgs: person,
+    });
+  }
+};
 
-                return person.save();            
-            }
-        },
-    
-        Person: {
-            age: (root) => new Date().getFullYear() - root.birthYear,
-            address: (root) => {
-                return {
-                    street: root.street,
-                    city: root.city
-                };
-            }
-        }
-    };
-    
+const resolvers = {
+  Query: {
+    personCount: async () => await Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
+      if (!args.phone) return await Person.find({});
+      return await Person.find({ phone: { $exists: args.phone === "YES" } });
+    },
+    findPerson: async (root, args) => {
+      return await Person.findOne({ name: args.name });
+    },
+  },
+
+  Mutation: {
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args });
+      return await savePersonData(person);
+    },
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name });
+
+      if (!person) return null;
+
+      person.phone = args.phone;
+
+      await savePersonData(person);
+
+      return person;
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      try {
+        await user.save();
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      }
+
+      return user;
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new UserInputError("wrong credentials");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
+    },
+  },
+
+  Person: {
+    age: (root) => new Date().getFullYear() - root.birthYear,
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
-    console.log(`Server ready at ${url}`);
+  console.log(`Server ready at ${url}`);
 });
